f731b307fbf84a51a66996c305a9c2b118595448
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-22 16:56:56 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
 #so it has a value.
 #its mass assignment if we are iterate over the various keys and values
 #more notes in spec
-  def initialize(a_side:,b_side:,c_side:)
+  def initialize(a_side,b_side,c_side)
     @a_side = a_side
     @b_side = b_side
     @c_side = c_side
